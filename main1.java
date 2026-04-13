e43f3f5d7bbf6d6289fbc0c9c6687d463111b7a9
Saidhon4ik <saidkhonabdullaev@gmail.com>
2026-04-13 18:36:38 +0500

    array of objects

--- main1.java
+++ main1.java
@@ -150,3 +150,30 @@
         
 //     }
 // }
+
+public class main1{
+    public static void main(String[] args)
+    {
+        // Car car1 = new Car("Mustang", "Red");
+        // Car car2 = new Car("Corverrete" , "Blue");
+        // Car car3 = new Car("Charger" , "Yellow");
+
+        // Car[] cars = {car1, car2, car3};
+
+        Car[] cars = {
+            new Car("Mustang", "Red"),
+            new Car("Corverrete" , "Blue"),
+            new Car("Charger" , "Yellow")
+
+        };
+
+        
+        for(Car car : cars){
+            car.color = "black";
+        }
+
+        for(Car car : cars){
+            car.drive();
+        }
+    }
+}
